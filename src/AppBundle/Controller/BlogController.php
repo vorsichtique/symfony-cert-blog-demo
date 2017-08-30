6be36538675bdd35fab18b36322f524a41e98a2e
Malu <malu@example.com>
2017-08-30 09:21:34 +0200

    basic blogpost entity + controlling

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\BlogPost;
 
 
 class BlogController extends Controller
@@ -14,7 +14,17 @@ class BlogController extends Controller
      * @Route("/blog/", name="blog_index")
      */
     public function indexAction(){
-        return new Response('hi');
+        $bp = new BlogPost();
+        $bp->setTitle('ahahaha');
+
+        $bp2 = new BlogPost();
+        $bp2->setTitle('ohohoho');
+
+      //  $em = $this->getDoctrine()->getManager();
+
+       // $bps = $em->findAll();
+
+        return $this->render('blog/index.html.twig', ['bps' => [$bp, $bp2]]);
     }
 
 }
---
+++ src/AppBundle/Entity/BlogPost.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace AppBundle\Entity;
+
+
+class BlogPost
+{
+    protected $title;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+}
